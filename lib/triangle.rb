c13cfb803a478b2c8397ab9a3ced47287132ffd4
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-08-24 19:47:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,33 +1,21 @@
 class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
   
-  def initialize(num1, num2, num3)
+  def initialize(n1, n2, n3)
     @kind = kind 
   end
   
-  # def equilateral(num1, num2, num3)
-  #   if num1 == num2 && num2 == num3 && num1 == num3
-  #     puts equilateral
-  #   else
-  #     puts false 
-  #   end
-  # end
   
-  
-  
-  def kind
-    :equilateral
-    :isosceles
-    :scalene
-  end
-  
-end
-
-def kind(n1,n2,n3)
-  if n1 == n2 ==n3
+  def kind(n1,n2,n3)
+  if n1 && n2 == n3
     puts :equilateral
   elsif n1 == n2, n3 != n2, n3 !=n1 
     puts :isosceles
   else n1 != n2, n2 != n3, n1 != n3 
- end
+    puts :scalene
+  end
 end
+  
+end
+
+
