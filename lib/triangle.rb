fa6f5856913c0aad7d438981784a00243f8e6b1f
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-08-24 19:54:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   def kind()
   if @n1 & @n2 == @n3
     :equilateral
-  elsif (@n1 == @n2) || (@n3 != @n2) || (@n3 != @n1) 
+  elsif (@n1 == @n2) || (@n3 == @n2) || (@n3 == @n1) 
     :isosceles
   else (@n1 != @n2) & (@n2 != @n3) & (@n1 != @n3) 
     :scalene
