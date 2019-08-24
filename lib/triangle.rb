fb615547992a73afa7ac4ff9cec7697710516cb8
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-08-24 19:32:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(num1, num2, num3)
+    @kind = kind 
+  end
+  
+  # def equilateral(num1, num2, num3)
+  #   if num1 == num2 && num2 == num3 && num1 == num3
+  #     puts equilateral
+  #   else
+  #     puts false 
+  #   end
+  # end
+  
+  
+  
+  def kind
+    
+  end
+  
 end
